ad2bc66c2fc4bbcc27d5443fa3defc19fac97cd7
devinfo <devinformationproject@gmail.com>
2026-06-13 13:39:35 +0200

    Creating the navbar for my homepage

---
+++ src/MainPage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../common/Navbar';
+
+const HomePage = () => {
+  return (
+    <>
+      <Navbar />
+      <div className="text-3xl">HomePage</div>
+    </>
+  );
+};
+
+export default HomePage;

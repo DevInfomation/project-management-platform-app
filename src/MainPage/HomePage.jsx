97d2b9f5df7b7c19d65c4f3f9f9649dc6a1239f4
devinfo <devinformationproject@gmail.com>
2026-06-14 16:43:24 +0200

    Removing navbar

--- src/MainPage/HomePage.jsx
+++ src/MainPage/HomePage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Navbar from '../common/Navbar';
 
 const HomePage = () => {
   return (
     <>
-      <Navbar />
       <div className="text-3xl">HomePage</div>
     </>
   );

267ad4635ea089d6ecaa6199659389049e31c1d0
devinfo <devinformationproject@gmail.com>
2026-06-16 23:06:53 +0200

    Creating a homepage

--- src/MainPage/HomePage.jsx
+++ src/MainPage/HomePage.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const HomePage = () => {
   return (
     <>
-      <div className="text-3xl">HomePage</div>
+      <main className='bg-[#0F172A] min-h-screen'>
+        <div className="w-full px-10 py-2">
+          <div className="flex">
+            <p className='text-white'>test1</p>
+            <p className='text-white'>test2</p>
+          </div>
+        </div>
+      </main>
     </>
   );
 };
